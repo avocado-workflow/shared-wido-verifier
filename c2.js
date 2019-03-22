6fc3044efe6adc35a34fd0f89011c9c5023d2dec
Michael <mykhaylo-@users.noreply.github.com>
2019-03-22 14:14:44 +0200

    button to delete meeting

--- c2.js
+++ c2.js
@@ -1,14 +1,29 @@
-console.log("Script to fix wido is injected. Let's do our magic now...");
+console.log("Admin Widget Script is injected. Let's see what we can do here...");
 
 (function() {
+    let command = sessionStorage.getItem('chromeadminwidgetoperation');
+    console.log("operation to execute:" + command);
+
+    switch(command) {
+        case 'fixWido' : 
+            fixWido();
+            break;
+        case 'delete' :
+            deleteProcessInstance();
+            break;
+        default: 
+            console.log("No command received. Exiting...");
+    }
+})();
+
+function fixWido() {
+    console.log("Fixing WIDO... ")
 
     var task = document.querySelector('dynamic-task-view');
     var piKey = task.ProcessInstance._Key;
     var token = localStorage.getItem('accessToken');
-
     //var baseUrl = document.querySelector('avocado-frame').appMeta.byKey('host');
     var baseUrl = 'https://1-dot-ao2prod-backend.appspot.com/v1'
-
 
     fetch(baseUrl + "/admin-toolbox/rebuild-shared-wido/" + piKey, {
             headers: {
@@ -23,4 +38,34 @@ console.log("Script to fix wido is injected. Let's do our magic now...");
             });
         })
         .catch(function(res) { console.log(res) });
-})();
+}
+
+
+function deleteProcessInstance() {
+    console.log("deleting process instance... ");
+
+    var task = document.querySelector('dynamic-task-view');
+    var piKey = task.ProcessInstance._Key;
+    var token = localStorage.getItem('accessToken');
+    var baseUrl = 'https://1-dot-ao2prod-backend.appspot.com/v1'
+
+    document.querySelector('main-container').$.modal.$['generic-modal'].close();
+    
+    task.fire('show-spinner');
+    
+    fetch(baseUrl + "/admin-toolbox/process-instance-mgmt/" + piKey, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            method: "DELETE"
+        })
+        .then(function(res) {
+            return res.text().then(function(text) {
+                console.log("Results: ");
+                console.log(text);
+                document.querySelector('dynamic-task-view').fire('change-route', '/meetings');
+            });
+        })
+        .catch(function(res) { console.log(res) });
+
+}
--- popup.js
+++ popup.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Extension popup is displayed and loaded...");
-    document.querySelector('#fixWido').addEventListener('click', fixWido);
+    document.querySelector('#fixWido')
+        .addEventListener('click', function() {executeOnPage('fixWido')});
+    document.querySelector('#deleteMeeting')
+        .addEventListener('click', function() {deleteMeeting()});
 });
 
-function fixWido() {
-    console.log("fix wido clicked");
+function deleteMeeting() {
+    console.log("deleteMeeting wido clicked");
+    var r = confirm("Are you sure you want to delete this Meeting completely and unrecoverably???");
+    if (r == true) {
+        console.log("DANGER! Deleting Process Instance...");
+        executeOnPage('delete');
+    } else {
+        console.log("Huh. Meeting is not deleted.");
+    }
+}
+
+function executeOnPage(operation) {
+    console.log("Command to execute on page: " + operation);
+
     chrome.tabs.executeScript({
-        file: 'injector.js'
+        code: 	"sessionStorage.setItem('chromeadminwidgetoperation','" + operation + "');"
+    }, function() {
+        chrome.tabs.executeScript({ file: 'injector.js' });
     });
 }
